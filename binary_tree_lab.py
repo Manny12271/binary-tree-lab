aaaaac67d4e6c56c2dcdcbbf62dd121339626299
manuel alfaro <manuel.alfaro247@gmail.com>
2026-02-17 14:18:27 -0600

    Complete binary tree lab

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -1,15 +1,24 @@
 from typing import Optional
 
+
 class TreeNode:
-    def __init__(self, val: int):
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None):
         self.val = val
-        self.left: Optional['TreeNode'] = None
-        self.right: Optional['TreeNode'] = None
+        self.left = left
+        self.right = right
 
-# TODO: Implement the max_depth function
+
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
 
-# TODO: Implement the lowest_common_ancestor function
+
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    if p.val < root.val and q.val < root.val:
+        return lowest_common_ancestor(root.left, p, q)
+    if p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+    return root
